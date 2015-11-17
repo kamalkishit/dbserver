f6932d2672756d3cd9a3f2ad92e933ab85465b7d
kamal <pandey.kishore@gmail.com>
2015-11-17 18:25:50 +0530

    more changes

--- src/main/java/com/humanize/dbserver/ExceptionConfig.java
+++ src/main/java/com/humanize/dbserver/ExceptionConfig.java
@@ -1,5 +1,25 @@
 package com.humanize.dbserver;
 
 public class ExceptionConfig {
+	
+	public static final int USER_CREATION_ERROR_CODE = 10001;
+	public static final int USER_UPDATION_ERROR_CODE = 10002;
+	public static final int USER_NOT_FOUND_ERROR_CODE = 10003;
+	public static final int USER_DELETION_ERROR_CODE = 10004;
+	
+	public static final int PAPER_CREATION_ERROR_CODE = 10005;
+	public static final int PAPER_UPDATION_ERROR_CODE = 10006;
+	public static final int PAPER_NOT_FOUND_ERROR_CODE = 10007;
+	public static final int PAPER_DELETION_ERROR_CODE = 10008;
+	
+	public static final String USER_CREATION_EXCEPTION = "User not created successfully";
+	public static final String USER_UPDATION_EXCEPTION = "User not updated successfully";
+	public static final String USER_NOT_FOUND_EXCEPTION = "User not found";
+	public static final String USER_DELETION_EXCEPTION = "User not deleted successfully";
+	
+	public static final String PAPER_CREATION_EXCEPTION = "Paper not created successfully";
+	public static final String PAPER_UPDATION_EXCEPTION = "Paper not updated successfully";
+	public static final String PAPER_NOT_FOUND_EXCEPTION = "Paper not found";
+	public static final String PAPER_DELETION_EXCEPTION = "Paper not deleted successfully";
 
 }
--- src/main/java/com/humanize/dbserver/service/UserRepositoryService.java
+++ src/main/java/com/humanize/dbserver/service/UserRepositoryService.java
@@ -3,8 +3,12 @@ package com.humanize.dbserver.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.humanize.dbserver.ExceptionConfig;
 import com.humanize.dbserver.dao.UserRepository;
 import com.humanize.dbserver.data.User;
+import com.humanize.dbserver.exception.UserCreationException;
+import com.humanize.dbserver.exception.UserNotFoundException;
+import com.humanize.dbserver.exception.UserUpdationException;
 
 @Service
 public class UserRepositoryService {
@@ -16,7 +20,7 @@ public class UserRepositoryService {
 		user = repository.save(user);
 		
 		if (user == null) {
-			
+			throw new UserCreationException(ExceptionConfig.USER_CREATION_ERROR_CODE, ExceptionConfig.USER_CREATION_EXCEPTION);
 		}
 		
 		return user;
@@ -26,7 +30,7 @@ public class UserRepositoryService {
 		user = repository.save(user);
 		
 		if (user == null) {
-			
+			throw new UserUpdationException(ExceptionConfig.USER_UPDATION_ERROR_CODE, ExceptionConfig.USER_UPDATION_EXCEPTION);
 		}
 		
 		return user;
@@ -36,7 +40,7 @@ public class UserRepositoryService {
 		User user = repository.findByEmailId(emailId);
 		
 		if (user == null) {
-			
+			throw new UserNotFoundException(ExceptionConfig.USER_NOT_FOUND_ERROR_CODE, ExceptionConfig.USER_NOT_FOUND_EXCEPTION);
 		}
 		
 		return user;
